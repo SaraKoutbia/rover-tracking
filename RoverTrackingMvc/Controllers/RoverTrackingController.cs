f1ec0caccb04ac50125c80dd6961f5adc6fd220c
sara el koutbia <elkoutbs@tcd.ie>
2020-12-09 01:14:57 -0500

    Adding a RoverTrackingController and the corresponding views.

---
+++ RoverTrackingMvc/Controllers/RoverTrackingController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using RoverTrackingService.Models;
+//using RoverTrackingService.Repositories;
+
+
+
+namespace RoverTrackingMvc.Controllers
+{
+    public class RoverTrackingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FinalPlateauState(input userInput)
+        {
+            var s = userInput.inputStr;
+            var plateau = new Plateau();
+
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //var plateauRegistery = new PlateauRepository();
+                    //plateau = plateauRegistery.computeFinalState(userInput);
+                    //ViewBag.Plateau = plateau;
+                    ViewData["Success"] = "Success";                    
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("Server side error occurred", ex.Message.ToString());
+                }
+            }
+            return View(userInput);
+        }
+    }
+}
