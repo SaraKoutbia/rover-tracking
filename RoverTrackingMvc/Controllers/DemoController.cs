7265032176f7570fe91193c4e8620375ecdc3e3f
sara el koutbia <elkoutbs@tcd.ie>
2020-12-12 01:22:03 -0500

    Adding a Demo controller.

---
+++ RoverTrackingMvc/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoverTrackingMvc.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
